18957f6b1f35cfa85896bb8566e488108db615ef
santi1703 <santi1703@gmail.com>
2019-05-22 11:12:47 -0300

    Update MovieController.php
    online testing

--- app/Http/Controllers/Api/MovieController.php
+++ app/Http/Controllers/Api/MovieController.php
@@ -18,7 +18,7 @@ class MovieController extends Controller
         if($request->has('title'))
             $movieQuery->orWhere('title', 'LIKE', '%' . $request->get('title') . '%');
 
-        dd($movieQuery);
+        dd($movieQuery->toSql());
         $movies = $movieQuery->orderBy('id')->get();
 
         return response()->json($movies);
